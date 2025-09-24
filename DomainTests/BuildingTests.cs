a38efad82a6a40eeb00407bc0a5669b51a88f867
firstmay <perevoschikov.s.m@firstmay.kirov.ru>
2025-09-24 13:31:36 +0300

    Добавил сущность Building и тесты

---
+++ DomainTests/BuildingTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using RPGTgBot.Domain.Entities;
+using RPGTgBot.Domain.ValueObjects;
+
+namespace Domain.Tests
+{
+    public class BuildingTests
+    {
+        [Fact]
+        public void Create_ShouldBeTrue_WhereCorrectedData()
+        {
+            var typeResult = BuildingType.Create("Ратуша");
+            if (!typeResult.IsSuccessAndNotNull())
+            {
+                return;
+            }
+
+            var buildingResult = Building.Create(0, typeResult.Value);
+
+            buildingResult.IsSuccess.Should().BeTrue();
+            buildingResult.Should().NotBeNull();
+            buildingResult.Value.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Create_ShouldBeFalse_WhereLevelLessThanOne()
+        {
+            var typeResult = BuildingType.Create("Ратуша");
+            if (!typeResult.IsSuccessAndNotNull())
+            {
+                return;
+            }
+
+            var buildingResult = Building.Create(0, typeResult.Value, 0);
+
+            buildingResult.IsSuccess.Should().BeFalse();
+            buildingResult.Should().NotBeNull();
+            buildingResult.Value.Should().BeNull();
+            buildingResult.Error.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Create_ShouldBeFalse_WhereBuildingTypeIsNull()
+        {
+            var buildingResult = Building.Create(0, null);
+
+            buildingResult.IsSuccess.Should().BeFalse();
+            buildingResult.Should().NotBeNull();
+            buildingResult.Value.Should().BeNull();
+            buildingResult.Error.Should().NotBeNull();
+        }
+    }
+}
---
+++ RPGTgBot.Domain/Entities/Building.cs
@@ -0,0 +1,33 @@
+﻿using RPGTgBot.Domain.ValueObjects;
+
+namespace RPGTgBot.Domain.Entities
+{
+    public class Building : Entity
+    {
+        public string Name { get; private set; }
+        public BuildingType BuildingType { get; private set; }
+        public int Level { get; private set; }
+
+        private Building(int id, BuildingType buildingType, int level)
+        {
+            SetId(id);
+            BuildingType = buildingType;
+            Level = level;
+        }
+
+        public static Result<Building> Create(int id, BuildingType buildingType, int level = 1)
+        {
+            if(level < 1)
+            {
+                return Result<Building>.Fail("Уровень не может быть меньше 1");
+            }
+            if (buildingType  == null)
+            {
+                return Result<Building>.Fail("Тип здания не определен.");
+            }
+
+            var building = new Building(id, buildingType, level);
+            return Result<Building>.Ok(building);
+        }
+    }
+}
