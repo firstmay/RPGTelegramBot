58dec8874974b9ca65bbfb30934f3421b95cff3a
firstmay <perevoschikov.s.m@firstmay.kirov.ru>
2025-09-24 13:30:12 +0300

    Добавлена базовая сущность Entity

---
+++ RPGTgBot.Domain/Entities/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGTgBot.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; private set; }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+    }
+}
