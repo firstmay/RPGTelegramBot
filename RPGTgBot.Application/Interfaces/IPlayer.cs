58e390e4a5e32bda65bc8d101b6c908ceee07bb6
firstmay <perevoschikov.s.m@firstmay.kirov.ru>
2025-09-24 15:39:56 +0300

    Добавлен интерфейс для получения информации об игроке

---
+++ RPGTgBot.Application/Interfaces/IPlayer.cs
@@ -0,0 +1,10 @@
+﻿using RPGTgBot.Application.DTOs;
+using RPGTgBot.Domain;
+
+namespace RPGTgBot.Application.Interfaces
+{
+    public interface IPlayer
+    {
+        Task<Result<PlayerProfileDto>> GetProfileAsync();
+    }
+}
