38030cd40a267aaa87114ae8dc8ca13bcd976a91
firstmay <perevoschikov.s.m@firstmay.kirov.ru>
2025-09-24 12:29:26 +0300

    Добавлен тип постройки(BuildingType)

---
+++ DomainTests/BuildingTypeTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using RPGTgBot.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Tests
+{
+    public class BuildingTypeTests
+    {
+        [Fact]
+        public void Created_ShouldBeFalse_WhereNameIsEmpty()
+        {
+            var typeResult = BuildingType.Create(string.Empty);
+
+            typeResult.IsSuccess.Should().BeFalse();
+            typeResult.Value.Should().BeNull();
+            typeResult.Error.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Created_ShouldBeFalse_WhereNameIsSpaces()
+        {
+            var typeResult = BuildingType.Create(" ");
+
+            typeResult.IsSuccess.Should().BeFalse();
+            typeResult.Value.Should().BeNull();
+            typeResult.Error.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Created_ShouldBeTrue_WhereNameIsCorrected()
+        {
+            string name = "Ратуша";
+            var typeResult = BuildingType.Create(name);
+
+            typeResult.IsSuccess.Should().BeTrue();
+            typeResult.Value.Should().NotBeNull();
+            typeResult.Value.Type.Should().Be(name);
+            typeResult.Error.Should().Be(string.Empty);
+        }
+    }
+}
---
+++ RPGTgBot.Domain/ValueObjects/BuildingType.cs
@@ -0,0 +1,21 @@
+﻿namespace RPGTgBot.Domain.ValueObjects
+{
+    public class BuildingType
+    {
+        public string Type { get; private set; }
+
+        private BuildingType(string type) 
+        {
+            Type = type;
+        }
+
+        public static Result<BuildingType> Create(string type)
+        {
+            if (string.IsNullOrWhiteSpace(type))
+                return Result<BuildingType>.Fail("Тип не может быть пустым.");
+            
+            var createdType = new BuildingType(type);
+            return Result<BuildingType>.Ok(createdType);
+        }
+    }
+}
