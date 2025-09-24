c47267030732f33be59720e49ec0ad00f19e8231
firstmay <perevoschikov.s.m@firstmay.kirov.ru>
2025-09-24 15:40:33 +0300

    добавлена сущность User для БД

---
+++ RPGTgBot.Infrastructure/DataBaseContext/RPGDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RPGTgBot.Infrastructure.Entities;
+
+
+namespace RPGTgBot.Infrastructure.DataBaseContext
+{
+    public class RPGDbContext : DbContext
+    {
+        public RPGDbContext(DbContextOptions<RPGDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(e =>
+            {
+                e.HasKey(u => u.Id);
+                e.Property(u => u.Name).IsRequired();
+                e.Property(u => u.RegistrationDate).IsRequired();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
