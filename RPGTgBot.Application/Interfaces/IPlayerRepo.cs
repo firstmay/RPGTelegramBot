83bebcebe6aa0f40fcb57e8168c5cc28fb76a56c
firstmay <perevoschikov.s.m@firstmay.kirov.ru>
2025-09-25 15:32:21 +0300

    Добавлен репозиторий для игрока и его интерфейс

---
+++ RPGTgBot.Application/Interfaces/IPlayerRepo.cs
@@ -0,0 +1,12 @@
+﻿using RPGTgBot.Application.DTOs;
+using RPGTgBot.Domain;
+using RPGTgBot.Domain.Entities;
+
+namespace RPGTgBot.Application.Interfaces
+{
+    public interface IPlayerRepo
+    {
+        Task<Result> AddAsync(Player player);
+        Task<Result<Player>> GetByIdAsync(long tgId);
+    }
+}
---
+++ RPGTgBot.Infrastructure/Repositories/PlayerRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RPGTgBot.Application.Interfaces;
+using RPGTgBot.Domain;
+using RPGTgBot.Domain.Entities;
+using RPGTgBot.Infrastructure.DataBaseContext;
+using RPGTgBot.Infrastructure.Entities;
+
+namespace RPGTgBot.Infrastructure.Repositories
+{
+    public class PlayerRepository(RPGDbContext context) : IPlayerRepo
+    {
+        private readonly RPGDbContext db = context;
+
+        public async Task<Result> AddAsync(Player player)
+        {
+            User user = User.ConvertFrom(player);
+            
+            await db.Users.AddAsync(user);
+            await db.SaveChangesAsync();
+            
+            return Result.Ok();
+        }
+
+        public async Task<Result<Player>> GetByIdAsync(long tgId)
+        {
+            var userResult = await db.Users.FirstOrDefaultAsync(u => u.TelegramId == tgId);
+            if (userResult == null)
+            {
+                return Result<Player>.Fail("Пользователь не найден");
+            }
+            
+            var convertedUser = userResult.ConvertToPlayer();
+            if (!convertedUser.IsSuccessAndNotNull())
+            {
+                return Result<Player>.Fail(convertedUser.Error);
+            }
+            
+            return Result<Player>.Ok(convertedUser.Value);
+        }
+    }
+}
